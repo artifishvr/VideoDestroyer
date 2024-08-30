94d415f18932363584eba2ac65b1f2441b3beab2
Arti <59352535+artifishvr@users.noreply.github.com>
2024-08-29 23:17:23 -0700

    ana

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,15 @@
   import "../app.css"; // Supports weights 100-900
   import "@fontsource-variable/dm-sans";
   import { Toaster } from "$lib/components/ui/sonner";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const analyticsScript = document.createElement("script");
+    analyticsScript.defer = true;
+    analyticsScript.dataset.domain = "destroy.arti.lol";
+    analyticsScript.src = "https://a.arti.lol/js/script.js";
+    document.head.appendChild(analyticsScript);
+  });
 </script>
 
 <Toaster />
